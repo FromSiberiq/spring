28ed311deabfd52fdad7183493a14d614601b52a
Okami <mr.naydyuk2@gmail.com>
2025-02-16 15:01:36 +0800

    Add edit request

--- src/main/java/com/example/spring/book/service/BookService.java
+++ src/main/java/com/example/spring/book/service/BookService.java
@@ -48,5 +48,10 @@ public class BookService {
         return book;
     }
 
-
+    public Optional<BookEntity> edit(BookEntity book) {
+        BookEntity oldBookEntity = byId(book.getId()).orElseThrow();
+        oldBookEntity.setTitle(book.getTitle());
+        oldBookEntity.setDescription(book.getDescription());
+        return Optional.of(oldBookEntity);
+    }
 }
